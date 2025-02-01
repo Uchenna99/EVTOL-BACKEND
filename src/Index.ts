4fc7eac9cb4e3d87c2d741288a05b73677d6b6b9
Uchenna99 <uchennaagbu@gmail.com>
2025-02-01 09:20:25 +0100

    setting up database connection

--- src/Index.ts
+++ src/Index.ts
@@ -1,16 +1,42 @@
-import { PrismaClient } from "@prisma/client";
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
 
-let db: PrismaClient;
 
-declare global {
-    var __db: PrismaClient | undefined;
+dotenv.config();
+
+const portEnv = process.env.PORT;
+if (!portEnv) {
+    console.error('Error: PORT is not defined in .env file')
+    process.exit(1);
 }
 
-if(!global.__db){
-    global.__db = new PrismaClient();
+const PORT: number = parseInt(portEnv, 10);
+if(isNaN(PORT)) {
+    console.error('Error: PORT is not a number in .env file');
+    process.exit(1);
 }
 
-db = global.__db;
+
+const app = express();
+
+const corsOptions = {
+    origin: '*',
+    credentilas: true,
+    allowedHeaders: '*',
+    methods: 'GET, POST, PUT, PATCH, DELETE, HEAD'
+}
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use('evtol/api/v1/users', )
 
 
-export { db };
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
+});
+
+export default app;
